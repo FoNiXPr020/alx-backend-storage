8b5110ba63ea24e850fd0960c7a71c82c599d13b
FoNiX <rogueman2018@gmail.com>
2024-07-10 08:22:45 +0100

    Adding Task 11 Need Meeting

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,6 @@
+-- Optimize search and score by first letter and score
+
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
