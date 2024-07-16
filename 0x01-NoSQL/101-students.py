264ef4cb7426b6a90696d060e1125456c89c99a8
FoNiX <rogueman2018@gmail.com>
2024-07-16 16:34:06 +0100

    Adding Task 101 🔍 Find Top Students 🕵🏽

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+    function documentation
+"""
+
+
+def top_students(mongo_collection):
+    """ function documentation """
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
