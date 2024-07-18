664b092d93d2d0c8b231bccd0394889d0add4c10
FoNiX <fonixpro12@gmail.com>
2024-07-17 20:05:42 -0700

    Updating web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,7 +6,6 @@ import requests
 import redis
 from functools import wraps
 
-# Initialize Redis connection
 store = redis.Redis()
 
 
@@ -18,17 +17,14 @@ def count_url_access(method):
         cached_key = "cached:" + url
         cached_data = store.get(cached_key)
         if cached_data:
-            print(f"Cache hit for {url}")
             return cached_data.decode("utf-8")
 
         count_key = "count:" + url
         html = method(url)
 
-        # Increment the access count and cache the HTML content
         store.incr(count_key)
         store.set(cached_key, html)
-        store.expire(cached_key, 10)  # Cache expires after 10 seconds
-        print(f"Cache miss for {url}. Fetching from the web.")
+        store.expire(cached_key, 10)
         return html
     return wrapper
 
@@ -38,12 +34,3 @@ def get_page(url: str) -> str:
     """ Returns HTML content of a url """
     res = requests.get(url)
     return res.text
-
-
-# Example usage
-if __name__ == "__main__":
-    test_url = ("http://slowwly.robertomurray.co.uk/delay/5000/"
-                "url/http://www.google.com")
-    print(get_page(test_url))
-    print(get_page(test_url))
-    print(get_page(test_url))
